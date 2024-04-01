948721907e2f5eb20e2a921bcabca1752b173237
Arthur Selingin <88356338+artusk8dev@users.noreply.github.com>
2024-04-01 15:18:46 -0300

    terminando efeito navbar

---
+++ src/scripts/efeitoNav.js
@@ -0,0 +1,18 @@
+document.querySelectorAll(".linksNav li a").forEach(function(item) {
+    item.addEventListener("mouseover", function() {
+        // Quando o hover é ativado, adiciona uma classe 'not-hover' a todos os outros itens
+        console.log('aaa')
+        document.querySelectorAll(".linksNav a").forEach(function(otherItem) {
+            if (otherItem !== item) {
+                otherItem.classList.add("notHover");
+            }
+        });
+    });
+
+    item.addEventListener("mouseout", function() {
+        // Quando o hover é desativado, remove a classe 'not-hover' de todos os itens
+        document.querySelectorAll(".linksNav li a").forEach(function(otherItem) {
+            otherItem.classList.remove("notHover");
+        });
+    });
+});
